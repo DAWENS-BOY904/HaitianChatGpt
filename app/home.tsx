2be5d12cb4284884474ac5833b589aa44862d6c5
newdawens-boop <newdawens@gmail.com>
2026-02-26 05:28:52 -0500

    Update home.tsx

--- app/home.tsx
+++ app/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+fix white screen make sure all error fix  React, { useState, useRef, useEffect, useCallback } from 'react';
 import { 
   View, 
   Text, 
