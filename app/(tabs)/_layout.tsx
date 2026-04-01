1b5dda6be5b86cecb0b48e9ca5815fdaa8e3405f
onspace.ai <onspace.ai@gmail.com>
2026-04-01 12:33:17 -0400

    Fix (tabs)/_layout.tsx syntax error

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -50,27 +50,3 @@ export default function TabLayout() {
     </Tabs>
   );
 }
-%o TypeError: Cannot read properties of undefined (reading 'fontSize') The above error occurred in the <ContextNavigator> component. React will try to recreate this component tree from scratch using the error boundary you provided, LogBoxStateSubscription.
-2026-04-01 12:27:34
-Dismiss
-Fix now
-Type:
-Uncaught Error
-File:
-app/(tabs)/_layout.tsx(6:2719)
-%o
-
-TypeError: Cannot read properties of undefined (reading 'fontSize')
-
-The above error occurred in the <ContextNavigator> component.
- React will try to recreate this component tree from scratch using the error boundary you provided, LogBoxStateSubscription.
-     at addLog (node_modules/.pnpm/@expo+metro-runtime@5.0.4_react-native@0.79.3_@babel+core@7.27.4_@types+react@19.0.14_react@19.0.0_/node_modules/@expo/metro-runtime/src/error-overlay/Data/LogBoxData.tsx:185:30)
-     at registerError (node_modules/.pnpm/@expo+metro-runtime@5.0.4_react-native@0.79.3_@babel+core@7.27.4_@types+react@19.0.14_react@19.0.0_/node_modules/@expo/metro-runtime/src/error-overlay/LogBox.web.ts:163:20)
-     at console.error (node_modules/.pnpm/@expo+metro-runtime@5.0.4_react-native@0.79.3_@babel+core@7.27.4_@types+react@19.0.14_react@19.0.0_/node_modules/@expo/metro-runtime/src/error-overlay/LogBox.web.ts:51:27)
-     at <global> (app/(tabs)/_layout.tsx:6:2719)
-     at defaultOnCaughtError (node_modules/.pnpm/react-dom@19.0.0_react@19.0.0/node_modules/react-dom/cjs/react-dom-client.development.js:7445:21)
-     at logCaughtError (node_modules/.pnpm/react-dom@19.0.0_react@19.0.0/node_modules/react-dom/cjs/react-dom-client.development.js:7484:9)
-     at runWithFiberInDEV (node_modules/.pnpm/react-dom@19.0.0_react@19.0.0/node_modules/react-dom/cjs/react-dom-client.development.js:543:16)
-     at update.callback (node_modules/.pnpm/react-dom@19.0.0_react@19.0.0/node_modules/react-dom/cjs/react-dom-client.development.js:7531:11)
-     at callCallback (node_modules/.pnpm/react-dom@19.0.0_react@19.0.0/node_modules/react-dom/cjs/react-dom-client.development.js:10680:16)
-...
