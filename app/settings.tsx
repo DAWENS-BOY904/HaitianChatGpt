de7745291abee94dfa0e584af6a9f3645a883e58
newdawens-boop <newdawens@gmail.com>
2026-04-03 14:25:55 -0400

    Update settings.tsx

--- app/settings.tsx
+++ app/settings.tsx
@@ -144,7 +144,7 @@ export default function SettingsScreen() {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#000000',
+      backgroundColor: '#111111',
     },
     // Header style tankou foto a
     header: {
