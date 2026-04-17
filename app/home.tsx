4c118b8b3d6151599f24bbd273ba441ae1cf84c8
newdawens-boop <newdawens@gmail.com>
2026-04-17 00:43:10 -0400

    Update home.tsx

--- app/home.tsx
+++ app/home.tsx
@@ -1634,3 +1634,5 @@ class ErrorBoundary extends Component<{ children: React.ReactNode }, { hasError:
     return this.props.children;
   }
 }
+Something went wrong
+Cannot access 'cleanedBeforeCard' before initialization
