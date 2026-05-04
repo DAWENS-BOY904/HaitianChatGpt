3df947a7af6809a44cd2bc701f7d8a24cfd2b2dc
newdawens-boop <newdawens@gmail.com>
2026-05-04 13:38:33 -0400

    Update MessageItem.tsx

--- components/MessageItem.tsx
+++ components/MessageItem.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useCallback, useRef, useEffect, memo } from 'react';
+fycking fix Element type is invalid: expected a string (for built-in components) or a class/function (for composite components)
+but got: undefined.import React, { useState, useCallback, useRef, useEffect, memo } from 'react';
 import {
   View,
   Text,
