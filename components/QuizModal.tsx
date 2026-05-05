e93354c9c05aa447d1ea1a15dd67c9af86515628
newdawens-boop <newdawens@gmail.com>
2026-05-05 10:45:35 -0400

    Update QuizModal.tsx

--- components/QuizModal.tsx
+++ components/QuizModal.tsx
@@ -514,3 +514,5 @@ const s = StyleSheet.create({
   },
   historyScoreText: { fontSize: 16, fontWeight: '700' },
 });
+
+add dark/light system also in preset modal and fix voice read in message action.
