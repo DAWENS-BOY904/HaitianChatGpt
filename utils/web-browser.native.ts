5f2e8fd4a99f70d484c639f5ebd892f1416848ab
newdawens-boop <newdawens@gmail.com>
2026-05-21 00:34:44 -0400

    Delete utils directory

--- utils/web-browser.native.ts
+++
@@ -1,5 +0,0 @@
-import * as ExpoWebBrowser from 'expo-web-browser';
-
-export const openBrowserAsync = ExpoWebBrowser.openBrowserAsync;
-export const dismissBrowser = ExpoWebBrowser.dismissBrowser;
-export default ExpoWebBrowser;
