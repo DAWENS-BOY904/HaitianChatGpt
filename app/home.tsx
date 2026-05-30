16b385c9c83bbc3a672120f7bd515c5b690ebb9c
onspace.ai <onspace.ai@gmail.com>
2026-05-29 21:31:30 -0400

    Code edited in OnSpace Code Editor

--- app/home.tsx
+++ app/home.tsx
@@ -3843,3 +3843,4 @@ class ErrorBoundary extends Component<{ children: React.ReactNode }, { hasError:
     return this.props.children;
   }
 }
+please ai put the bottom sheet menu anba message lan kolel anba user messge press long.
